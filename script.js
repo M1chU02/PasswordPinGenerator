5a79e4ebea2b9c3a760749db3232d641ff602618
M1chU02 <115975356+M1chU02@users.noreply.github.com>
2023-08-03 12:01:02 +0200

    script.js

--- script.js
+++ script.js
@@ -71,16 +71,12 @@ function generatePassword(lower, upper, number, symbol, length) {
 }
 
 function shufflePassword(finalPassword) {
-  // Convert the password string into an array of characters
   const charArray = finalPassword.split("");
 
-  // Fisher-Yates shuffle algorithm to randomly reorder the characters
   for (let i = charArray.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [charArray[i], charArray[j]] = [charArray[j], charArray[i]];
   }
-
-  // Convert the shuffled array back to a string
   return charArray.join("");
 }
 
